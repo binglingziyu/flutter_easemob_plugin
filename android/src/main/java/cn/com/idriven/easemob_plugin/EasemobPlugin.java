330755907b4c2b5af68e18804e48e09cf7c309ef
hubin <hubin.android@gmail.com>
2019-03-28 19:00:29 +0800

    test easemob get coversation list;

--- android/src/main/java/cn/com/idriven/easemob_plugin/EasemobPlugin.java
+++ android/src/main/java/cn/com/idriven/easemob_plugin/EasemobPlugin.java
@@ -2,12 +2,26 @@ package cn.com.idriven.easemob_plugin;
 
 import android.util.Log;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.hyphenate.EMCallBack;
+import com.hyphenate.EMConnectionListener;
+import com.hyphenate.EMError;
+import com.hyphenate.EMMessageListener;
+import com.hyphenate.EMValueCallBack;
 import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMConversation;
+import com.hyphenate.chat.EMGroup;
 import com.hyphenate.chat.EMMessage;
 import com.hyphenate.chat.EMOptions;
+import com.hyphenate.exceptions.HyphenateException;
+import com.hyphenate.util.NetUtils;
+
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 import cn.com.idriven.easemob_plugin.util.StringUtils;
 import io.flutter.plugin.common.MethodCall;
@@ -21,15 +35,18 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 public class EasemobPlugin implements MethodCallHandler {
 
   private final PluginRegistry.Registrar registrar;
+  private final MethodChannel channel;
 
-  public EasemobPlugin(PluginRegistry.Registrar registrar) {
+  public EasemobPlugin(PluginRegistry.Registrar registrar, MethodChannel channel) {
     this.registrar = registrar;
+    this.channel = channel;
+    JSON.DEFAULT_GENERATE_FEATURE |= SerializerFeature.DisableCircularReferenceDetect.mask;
   }
 
   /** Plugin registration. */
   public static void registerWith(Registrar registrar) {
     final MethodChannel channel = new MethodChannel(registrar.messenger(), "easemob_plugin");
-    channel.setMethodCallHandler(new EasemobPlugin(registrar));
+    channel.setMethodCallHandler(new EasemobPlugin(registrar, channel));
   }
 
   @Override
@@ -37,134 +54,18 @@ public class EasemobPlugin implements MethodCallHandler {
     try {
       switch (call.method) {
         case "init":
-          String appKey = call.argument("appKey");
-          Boolean acceptInvitationAlways = call.argument("acceptInvitationAlways");
-          if(acceptInvitationAlways == null) {
-            acceptInvitationAlways = false;
-          }
-          Boolean autoAcceptGroupInvitation = call.argument("autoAcceptGroupInvitation");
-          if(autoAcceptGroupInvitation == null) {
-            autoAcceptGroupInvitation = false;
-          }
-          Boolean autoTransferMessageAttachments = call.argument("autoTransferMessageAttachments");
-          if(autoTransferMessageAttachments == null) {
-            autoTransferMessageAttachments = false;
-          }
-          Boolean autoDownloadThumbnail = call.argument("autoDownloadThumbnail");
-          if(autoDownloadThumbnail == null) {
-            autoDownloadThumbnail = false;
-          }
-          Boolean allowChatroomOwnerLeave = call.argument("allowChatroomOwnerLeave");
-          if(allowChatroomOwnerLeave == null) {
-            allowChatroomOwnerLeave = true;
-          }
-          Boolean deleteMessagesAsExitGroup = call.argument("deleteMessagesAsExitGroup");
-          if(deleteMessagesAsExitGroup == null) {
-            deleteMessagesAsExitGroup = true;
-          }
-          Boolean useFCM = call.argument("useFCM");
-          if(useFCM == null) {
-            useFCM = false;
-          }
-          Boolean usingHttpsOnly = call.argument("usingHttpsOnly");
-          if(usingHttpsOnly == null) {
-            usingHttpsOnly = false;
-          }
-          Boolean autoLogin = call.argument("autoLogin");
-          if(autoLogin == null) {
-            autoLogin = true;
-          }
-          Boolean debugMode = call.argument("debugMode");
-          if(debugMode == null) {
-            debugMode = false;
-          }
-          EMOptions options = new EMOptions();
-          // 设置appkey
-          if(StringUtils.isNotBlank(appKey)) {
-            options.setAppKey(appKey);
-          }
-          // 默认添加好友时，是不需要验证的 true: 需要  false: 不需要
-          options.setAcceptInvitationAlways(acceptInvitationAlways);
-          // 设置是否自动接受加群邀请
-          options.setAutoAcceptGroupInvitation(autoAcceptGroupInvitation);
-          // 是否自动将消息附件上传到环信服务器，默认为True是使用环信服务器上传下载，如果设为 false，需要开发者自己处理附件消息的上传和下载
-          options.setAutoTransferMessageAttachments(autoTransferMessageAttachments);
-          // 是否自动下载附件类消息的缩略图等，默认为 true 这里和上边这个参数相关联
-          options.setAutoDownloadThumbnail(autoDownloadThumbnail);
-          // 设置是否允许聊天室owner离开并删除会话记录，意味着owner再不会受到任何消息
-          options.allowChatroomOwnerLeave(allowChatroomOwnerLeave);
-          // 设置退出(主动和被动退出)群组时是否删除聊天消息
-          options.setDeleteMessagesAsExitGroup(deleteMessagesAsExitGroup);
-          // 设置是否开启谷歌FCM推送
-          options.setUseFCM(useFCM);
-          // 使用https进行REST操作，默认值是false。
-          options.setUsingHttpsOnly(usingHttpsOnly);
-          // 设置自动登录
-          options.setAutoLogin(autoLogin);
-          //初始化
-          EMClient.getInstance().init(registrar.context().getApplicationContext(), options);
-          // 在做打包混淆时，关闭debug模式，避免消耗不必要的资源
-          EMClient.getInstance().setDebugMode(debugMode);
-          result.success(true);
-          Log.i("easemob", "环信初始化完成。");
+          easeMobInit(call, result);
           break;
         case "login":
-          String id = call.argument("id");
-          String password = call.argument("password");
-          Log.i("easemob", "id="+id);
-          Log.i("easemob", "password="+password);
-          if(StringUtils.isBlank(id)) {
-            result.error("id 为空", "用户 id 不能为空", null);
-            return;
-          }
-          if(StringUtils.isBlank(password)) {
-            result.error("密码为空", "用户密码不能为空", null);
-            return;
-          }
-          EMClient.getInstance().login(id, password, new EMCallBack() {//回调
-            @Override
-            public void onSuccess() {
-              EMClient.getInstance().groupManager().loadAllGroups();
-              EMClient.getInstance().chatManager().loadAllConversations();
-              result.success(true);
-
-              Log.i("easemob", "登录成功");
-            }
-
-            @Override
-            public void onProgress(int progress, String status) {
-
-            }
-
-            @Override
-            public void onError(int code, String message) {
-              Log.d("main", "登录聊天服务器失败！");
-              result.error("登录失败", message, null);
-              Log.i("easemob", "登录失败:"+message);
-            }
-          });
+          easeMobLogin(call, result);
           break;
         case "logout":
-          Boolean unbindToken = call.argument("unbindToken");
-          if(unbindToken == null) {
-            unbindToken = true;
-          }
-          EMClient.getInstance().logout(unbindToken, new EMCallBack() {
-            @Override
-            public void onSuccess() {
-              result.success(true);
-            }
-
-            @Override
-            public void onError(int code, String error) {
-
-            }
-
-            @Override
-            public void onProgress(int progress, String status) {
-              result.error("退出失败", status, null);
-            }
-          });
+          easeMobLogout(call, result);
+          break;
+        case "getAllConversations":
+          Map<String, EMConversation> allConversations = EMClient.getInstance().chatManager().getAllConversations();
+          Log.i("easemob", "会话内容："+ JSON.toJSONString(allConversations));
+          result.success(JSON.toJSONString(allConversations));
           break;
         default:
           result.notImplemented();
@@ -174,4 +75,204 @@ public class EasemobPlugin implements MethodCallHandler {
       result.error("IOException encountered", call.method, e);
     }
   }
+
+  /**
+   * 环信初始化
+   * @param call MethodCall
+   * @param result Result
+   */
+  private void easeMobInit(MethodCall call, Result result) {
+    String appKey = call.argument("appKey");
+    boolean acceptInvitationAlways = getBooleanArgument(call, "acceptInvitationAlways", false);
+    boolean autoAcceptGroupInvitation = getBooleanArgument(call, "autoAcceptGroupInvitation", false);
+    boolean autoTransferMessageAttachments = getBooleanArgument(call, "autoTransferMessageAttachments", false);
+    boolean autoDownloadThumbnail = getBooleanArgument(call, "autoDownloadThumbnail", false);
+    boolean allowChatroomOwnerLeave = getBooleanArgument(call, "allowChatroomOwnerLeave", true);
+    boolean deleteMessagesAsExitGroup = getBooleanArgument(call, "deleteMessagesAsExitGroup", true);
+    boolean useFCM = getBooleanArgument(call, "useFCM", false);
+    boolean usingHttpsOnly = getBooleanArgument(call, "usingHttpsOnly", false);
+    boolean autoLogin = getBooleanArgument(call, "autoLogin", true);
+    boolean debugMode = getBooleanArgument(call, "debugMode", false);
+    EMOptions options = new EMOptions();
+    // 设置appkey
+    if(StringUtils.isNotBlank(appKey)) {
+      options.setAppKey(appKey);
+    }
+    // 默认添加好友时，是不需要验证的 true: 需要  false: 不需要
+    options.setAcceptInvitationAlways(acceptInvitationAlways);
+    // 设置是否自动接受加群邀请
+    options.setAutoAcceptGroupInvitation(autoAcceptGroupInvitation);
+    // 是否自动将消息附件上传到环信服务器，默认为True是使用环信服务器上传下载，如果设为 false，需要开发者自己处理附件消息的上传和下载
+    options.setAutoTransferMessageAttachments(autoTransferMessageAttachments);
+    // 是否自动下载附件类消息的缩略图等，默认为 true 这里和上边这个参数相关联
+    options.setAutoDownloadThumbnail(autoDownloadThumbnail);
+    // 设置是否允许聊天室owner离开并删除会话记录，意味着owner再不会受到任何消息
+    options.allowChatroomOwnerLeave(allowChatroomOwnerLeave);
+    // 设置退出(主动和被动退出)群组时是否删除聊天消息
+    options.setDeleteMessagesAsExitGroup(deleteMessagesAsExitGroup);
+    // 设置是否开启谷歌FCM推送
+    options.setUseFCM(useFCM);
+    // 使用https进行REST操作，默认值是false。
+    options.setUsingHttpsOnly(usingHttpsOnly);
+    // 设置自动登录
+    options.setAutoLogin(autoLogin);
+    //初始化
+    EMClient.getInstance().init(registrar.context().getApplicationContext(), options);
+    // 在做打包混淆时，关闭debug模式，避免消耗不必要的资源
+    EMClient.getInstance().setDebugMode(debugMode);
+    result.success(true);
+    Log.i("easemob", "环信初始化完成。");
+
+    try {
+      EMClient.getInstance().groupManager().asyncGetJoinedGroupsFromServer(new EMValueCallBack<List< EMGroup >>() {
+        @Override
+        public void onSuccess(List<EMGroup> value) {
+        }
+        @Override
+        public void onError(int error, String errorMsg) {
+        }
+      });
+    } catch (Exception e) {
+      Log.i("easemob", "获取已加入的群失败, reason:" + e.getMessage());
+    }
+
+    // 注册一个监听连接状态的listener
+    EMClient.getInstance().addConnectionListener(new MyConnectionListener());
+    // 注册消息监听来接收消息。
+    EMClient.getInstance().chatManager().addMessageListener(msgListener);
+  }
+
+  /**
+   * 环信登录
+   * @param call MethodCall
+   * @param result Result
+   */
+  private void easeMobLogin(MethodCall call, final Result result) {
+    String id = call.argument("id");
+    String password = call.argument("password");
+    Log.i("easemob", "id="+id);
+    Log.i("easemob", "password="+password);
+    if(StringUtils.isBlank(id)) {
+      result.error("id 为空", "用户 id 不能为空", null);
+      return;
+    }
+    if(StringUtils.isBlank(password)) {
+      result.error("密码为空", "用户密码不能为空", null);
+      return;
+    }
+    EMClient.getInstance().login(id, password, new EMCallBack() {//回调
+      @Override
+      public void onSuccess() {
+        EMClient.getInstance().groupManager().loadAllGroups();
+        EMClient.getInstance().chatManager().loadAllConversations();
+        result.success(true);
+
+        Log.i("easemob", "登录成功");
+      }
+
+      @Override
+      public void onProgress(int progress, String status) {
+
+      }
+
+      @Override
+      public void onError(int code, String message) {
+        Log.d("main", "登录聊天服务器失败！");
+        result.error("登录失败", message, null);
+        Log.i("easemob", "登录失败:"+message);
+      }
+    });
+  }
+
+  /**
+   * 环信退出登录
+   * @param call MethodCall
+   * @param result Result
+   */
+  private void easeMobLogout(MethodCall call, final Result result) {
+    Boolean unbindToken = call.argument("unbindToken");
+    if(unbindToken == null) {
+      unbindToken = true;
+    }
+    EMClient.getInstance().logout(unbindToken, new EMCallBack() {
+      @Override
+      public void onSuccess() {
+        result.success(true);
+      }
+
+      @Override
+      public void onError(int code, String error) {
+        result.error("退出失败", error, null);
+      }
+
+      @Override
+      public void onProgress(int progress, String status) {}
+    });
+  }
+
+  private boolean getBooleanArgument(MethodCall call, String key, boolean defaultValue) {
+    Boolean value = call.argument(key);
+    return value == null ? defaultValue : value;
+  }
+
+  //
+  private EMMessageListener msgListener = new EMMessageListener() {
+
+    @Override
+    public void onMessageReceived(List<EMMessage> messages) {
+      //收到消息
+    }
+
+    @Override
+    public void onCmdMessageReceived(List<EMMessage> messages) {
+      //收到透传消息
+    }
+
+    @Override
+    public void onMessageRead(List<EMMessage> messages) {
+      //收到已读回执
+    }
+
+    @Override
+    public void onMessageDelivered(List<EMMessage> message) {
+      //收到已送达回执
+    }
+    @Override
+    public void onMessageRecalled(List<EMMessage> messages) {
+      //消息被撤回
+    }
+
+    @Override
+    public void onMessageChanged(EMMessage message, Object change) {
+      //消息状态变动
+    }
+  };
+
+  //实现ConnectionListener接口
+  private class MyConnectionListener implements EMConnectionListener {
+    @Override
+    public void onConnected() {
+    }
+    @Override
+    public void onDisconnected(final int error) {
+          if(error == EMError.USER_REMOVED){
+            // 显示帐号已经被移除
+          }else if (error == EMError.USER_LOGIN_ANOTHER_DEVICE) {
+            // 显示帐号在其他设备登录
+          } else {
+            if (NetUtils.hasNetwork(registrar.context())) {
+              //连接不到聊天服务器
+
+            } else {
+              //当前网络不可用，请检查网络设置
+            }
+          }
+        }
+  }
+
+  private void test() {
+//    EMClient.getInstance().groupManager().
+//            .getJoinedGroupsFromServer();
+  }
+
 }
--- lib/easemob_plugin.dart
+++ lib/easemob_plugin.dart
@@ -18,8 +18,7 @@ class EasemobPlugin {
     return await _channel.invokeMethod("logout");
   }
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  static Future<String> getAllConversations() async {
+    return await _channel.invokeMethod("getAllConversations");
   }
 }
